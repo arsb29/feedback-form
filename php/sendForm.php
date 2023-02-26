cdaa387304152fde19a419b4d21a462ac536e141
Arseny Belyaev <ars-bel@mail.ru>
2023-02-26 18:33:44 +0300

    insert into db

--- php/sendForm.php
+++ php/sendForm.php
@@ -1,10 +1,14 @@
 <?php
-// Формируем массив для JSON ответа
-$result = array(
-    'name' => 1,
-    'phonenumber' => 2
-);
+$mysqli = new mysqli("db4free.net:3306", "belyaev", "12345678", "feedback");
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
 
+$sql = "INSERT INTO clients VALUES ('" . $_POST["fio"] . "');";
+$result = $mysqli -> query($sql);
+$mysqli->close();
 // Переводим массив в JSON
-echo json_encode($result);
+echo json_encode(array('result' => $result));
 ?>
